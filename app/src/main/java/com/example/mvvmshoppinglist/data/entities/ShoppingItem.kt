80f27e43ba6d7bba6888f2d3282be53c4761a647
Mauro Crispim <mcrispim@hotmail.com>
2023-06-06 21:20:09 -0300

    Corrects ShoppingItem entity

--- app/src/main/java/com/example/mvvmshoppinglist/data/entities/ShoppingItem.kt
+++ app/src/main/java/com/example/mvvmshoppinglist/data/entities/ShoppingItem.kt
@@ -3,10 +3,11 @@ package com.example.mvvmshoppinglist.data.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity()
+@Entity(tableName = "shopping_items")
 data class ShoppingItem(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val name: String,
+    var name: String,
     var amount: Int
-)
+) {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+}
