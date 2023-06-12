57cf7c3a4fb7ff6d1a3bc17f29ac0de362ba5ec5
Mauro Crispim <mcrispim@hotmail.com>
2023-06-12 08:12:56 -0300

    Corrects repo by passing only the dao as dependency

--- app/src/main/java/com/example/mvvmshoppinglist/data/repositories/ShoppingItemRepository.kt
+++ app/src/main/java/com/example/mvvmshoppinglist/data/repositories/ShoppingItemRepository.kt
@@ -1,15 +1,12 @@
 package com.example.mvvmshoppinglist.data.repositories
 
 import androidx.lifecycle.LiveData
-import com.example.mvvmshoppinglist.data.db.ShoppingDb
 import com.example.mvvmshoppinglist.data.db.ShoppingItemDao
 import com.example.mvvmshoppinglist.data.entities.ShoppingItem
 
 class ShoppingItemRepository(
-    val shoppingDatabase: ShoppingDb
+    val shoppingDao: ShoppingItemDao
 ) {
-    private val shoppingDao = shoppingDatabase.getShoppingItemDao()
-
     suspend fun upsertItem(item: ShoppingItem) {
         shoppingDao.upsertShoppingItem(item)
     }
